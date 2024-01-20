01d24d0e02b4fa59949f8cd21f5c3d5b4fa3169f
justSmK <28645140+justSmK@users.noreply.github.com>
2024-01-20 13:49:41 +0300

    Create Pagination Indicator

---
+++ RickAndMorty/Views/RMFooterLoadingCollectionReusableView.swift
@@ -0,0 +1,43 @@
+//
+//  RMFooterLoadingCollectionReusableView.swift
+//  RickAndMorty
+//
+//  Created by Sergei Semko on 1/20/24.
+//
+
+import UIKit
+
+final class RMFooterLoadingCollectionReusableView: UICollectionReusableView {
+    static let identifier = "RMFooterLoadingCollectionReusableView"
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        addSubview(spinner)
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
+    }
+    
+    public func startAnimating() {
+        spinner.startAnimating()
+    }
+}
