31b4381b88d43c0c8bb024da20ebb3bd84e65e1f
justSmK <28645140+justSmK@users.noreply.github.com>
2024-01-21 21:13:08 +0300

    Start creating a Detail View

--- RickAndMorty/Controllers/Onter/RMCharacterDetailViewController.swift
+++ RickAndMorty/Controllers/Onter/RMCharacterDetailViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 final class RMCharacterDetailViewController: UIViewController {
     
     private let viewModel: RMCharacterDetailViewViewModel
+    
+    private let detailView = RMCharacterDetailView()
+    
+    // MARK: - Init
 
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
@@ -27,6 +31,29 @@ final class RMCharacterDetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(detailView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(didTapShare)
+        )
+        addConstraints()
+//        viewModel.fetchCharacterData()
+    }
+    
+    @objc
+    private func didTapShare(_ sender: UIBarButtonItem) {
+        // Share character info
+        print(sender)
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            detailView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 
 }
--- RickAndMorty/Views/RMCharacterDetailView.swift
+++ RickAndMorty/Views/RMCharacterDetailView.swift
@@ -7,6 +7,62 @@
 
 import UIKit
 
+/// View for single character info
 final class RMCharacterDetailView: UIView {
+    
+    private var collectionView: UICollectionView?
+    
+    private let spinner: UIActivityIndicatorView =  {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
+    // MARK: - Init
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemPurple
+        let collectionView = createCollectionView()
+        self.collectionView = collectionView
+        addSubviews(collectionView, spinner)
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+    
+    private func addConstraints() {
+        guard let collectionView else {
+            return
+        }
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+    
+    private func createCollectionView() -> UICollectionView {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, _ in
+            return self.createSection(for: sectionIndex)
+        }
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self,
+                                forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }
+    
+    private func createSection(for sectionIndex: Int) -> NSCollectionLayoutSection {
+        
+    }
 }
