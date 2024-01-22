8152590b6431c67cf2f6d70b91450c206c501e6f
justSmK <28645140+justSmK@users.noreply.github.com>
2024-01-22 13:00:34 +0300

    Create Compositional Layout

--- RickAndMorty/Controllers/Onter/RMCharacterDetailViewController.swift
+++ RickAndMorty/Controllers/Onter/RMCharacterDetailViewController.swift
@@ -12,12 +12,13 @@ final class RMCharacterDetailViewController: UIViewController {
     
     private let viewModel: RMCharacterDetailViewViewModel
     
-    private let detailView = RMCharacterDetailView()
+    private let detailView: RMCharacterDetailView
     
     // MARK: - Init
 
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
+        self.detailView = RMCharacterDetailView(frame: .zero, viewModel: viewModel)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -38,6 +39,8 @@ final class RMCharacterDetailViewController: UIViewController {
             action: #selector(didTapShare)
         )
         addConstraints()
+        detailView.collectionView?.delegate = self
+        detailView.collectionView?.dataSource = self
 //        viewModel.fetchCharacterData()
     }
     
@@ -56,4 +59,31 @@ final class RMCharacterDetailViewController: UIViewController {
         ])
     }
 
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+
+extension RMCharacterDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return viewModel.sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        if indexPath.section == 0 {
+            cell.backgroundColor = .systemPink
+        } else if indexPath.section == 1 {
+            cell.backgroundColor = .systemGreen
+        } else {
+            cell.backgroundColor = .systemBlue
+        }
+        return cell
+    }
+    
+    
 }
