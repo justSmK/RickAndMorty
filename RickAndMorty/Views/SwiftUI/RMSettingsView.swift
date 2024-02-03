c3162be0892cb35c872da68de9958a2ee6648262
justSmK <28645140+justSmK@users.noreply.github.com>
2024-02-03 19:29:29 +0300

    Create SwiftUI Settings View

---
+++ RickAndMorty/Views/SwiftUI/RMSettingsView.swift
@@ -0,0 +1,45 @@
+//
+//  RMSettingsView.swift
+//  RickAndMorty
+//
+//  Created by Sergei Semko on 2/3/24.
+//
+
+import SwiftUI
+
+struct RMSettingsView: View {
+    
+    let viewModel: RMSettingsViewViewModel
+    
+    init(viewModel: RMSettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        List(viewModel.cellViewModels) { viewModel in
+            HStack {
+                if let image = viewModel.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(.white)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.red)
+                        .padding(8)
+                        .background(Color(viewModel.iconContainerColor))
+                        .cornerRadius(6)
+                }
+                Text(viewModel.title)
+                    .padding(.leading, 10)
+            }
+            .padding(.bottom, 3)
+        }
+    }
+}
+
+#Preview {
+    RMSettingsView(viewModel: .init(cellViewModels: RMSettingsOption.allCases.compactMap({
+        return RMSettingsCellViewModel(type: $0)
+    })))
+}
