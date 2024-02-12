267a41b9705294b0d331ff4e9e1cf7abe261e988
justSmK <28645140+justSmK@users.noreply.github.com>
2024-02-12 21:20:50 +0300

    Create Search Request Builder

--- RickAndMorty/ViewModels/Search/RMSearchInputViewViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMSearchInputViewViewModel.swift
@@ -16,6 +16,17 @@ final class RMSearchInputViewViewModel {
         case gender = "Gender"
         case locationType = "Location type"
         
+        var queryArgument: String {
+            switch self {
+            case .status:
+                return "status"
+            case .gender:
+                return "gender"
+            case .locationType:
+                return "type"
+            }
+        }
+        
         var choices: [String] {
             switch self {
             case .status:
