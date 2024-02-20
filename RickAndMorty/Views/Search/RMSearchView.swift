83b4a038beb68fe118a4bf6e78a3db855c73bff7
justSmK <28645140+justSmK@users.noreply.github.com>
2024-02-20 17:41:35 +0300

    Create Search Bar Text

--- RickAndMorty/Views/Search/RMSearchView.swift
+++ RickAndMorty/Views/Search/RMSearchView.swift
@@ -72,11 +72,17 @@ final class RMSearchView: UIView {
 // MARK: - RMSearchInputViewDelegate
 
 extension RMSearchView: RMSearchInputViewDelegate {
+    func rmSearchInputViewDidTapSearchKeyboardButton(_ inputView: RMSearchInputView) {
+        viewModel.executeSearch()
+    }
+    
+    func rmSearchInputView(_ inputView: RMSearchInputView, didChangeSearchText text: String) {
+        viewModel.set(query: text)
+    }
+    
     func rmSearchInputView(_ inputView: RMSearchInputView, didSelectOption option: RMSearchInputViewViewModel.DynamicOption) {
         delegate?.rmSearchView(self, didSelectOption: option)
     }
-    
-    
 }
 
 // MARK: - CollectionView: UICollectionViewDelegate, UICollectionViewDataSource
